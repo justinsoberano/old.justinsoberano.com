068f750a9f42839a746878534ecba63db6593161
System <system@noreply.github.com>
2024-07-04 14:03:04 -0700

    Fixed Typescript version incompatibility error

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./components/styles/global_stylesheet.css"
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
 	<React.StrictMode>
-		<div style={{ width: "100svw", height: "100svh" }}>
+		<div style={{ width: "100svw", height: "100svh"}}>
 			<App />
 		</div>
 	</React.StrictMode>
