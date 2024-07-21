8e073affa8345c758d744daf60629e3e22ba6839
System <system@noreply.github.com>
2024-07-20 19:01:50 -0700

    eek

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,12 @@ import { useEffect } from "react";
 import Background from "./components/canvas/Canvas";
 
 declare const InstallTrigger: any;
-
 export default function App() {
-
   useEffect(() => {
     const isFirefox = typeof InstallTrigger !== 'undefined';
     if (isFirefox) {
       alert("This website is not optimized for Firefox. Currently working on a fix :)")
     }
   }, []);
-
-  return (
-    <>
-      <Background />
-    </>
-  );
+  return <Background />;
 }
